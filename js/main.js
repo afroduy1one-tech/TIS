287d10cb331fb2159d559df59e3999642157ec73
afroduy1one-tech <afroduy1one@gmail.com>
2026-07-13 17:28:25 +0300

    слайдер

--- js/main.js
+++ js/main.js
@@ -4,3 +4,48 @@ const menu = document.querySelector('.menu');
 burger.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+const slider = document.querySelector('.cards-slider');
+
+const nextBtn = document.querySelector('.slider-next');
+const prevBtn = document.querySelector('.slider-prev');
+
+
+let position = 0;
+
+
+nextBtn.addEventListener('click', () => {
+
+    position++;
+
+    slider.style.transform =
+    `translateX(-${position * 33.333}%)`;
+
+});
+
+
+prevBtn.addEventListener('click', () => {
+
+    position--;
+
+    if(position < 0){
+        position = 0;
+    }
+
+    slider.style.transform =
+    `translateX(-${position * 33.333}%)`;
+
+});
+const maxPosition = 4;
+
+
+nextBtn.addEventListener('click', () => {
+
+    if(position < maxPosition){
+        position++;
+    }
+
+    slider.style.transform =
+    `translateX(-${position * 33.333}%)`;
+
+});
