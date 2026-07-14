26a4fd24ee097834c35a97d0d7a63239e19007f2
afroduy1one-tech <afroduy1one@gmail.com>
2026-07-14 19:11:24 +0700

    до адаптива

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+fetch('modal.html')
+.then(response => response.text())
+.then(data => {
+
+    document.querySelector('#modal-container').innerHTML = data;
+
+    initModal();
+
+});
+
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 
@@ -7,18 +17,16 @@ burger.addEventListener('click', () => {
 const slider = document.querySelector('.cards-slider');
 const cards = document.querySelectorAll('.cards-slider .card');
 
-const nextBtn = document.querySelector('.slider-next');
-const prevBtn = document.querySelector('.slider-prev');
-
+const sliderNextBtn = document.querySelector('.slider-next');
+const sliderPrevBtn = document.querySelector('.slider-prev');
 
 let position = 0;
-
 
 const cardWidth = cards[0].offsetWidth + 40;
 const maxPosition = cards.length - 3; // 7 карточек, показываем 3
 
 
-nextBtn.addEventListener('click', () => {
+sliderNextBtn.addEventListener('click', () => {
 
     if(position < maxPosition){
         position++;
@@ -29,8 +37,7 @@ nextBtn.addEventListener('click', () => {
 
 });
 
-
-prevBtn.addEventListener('click', () => {
+sliderPrevBtn.addEventListener('click', () => {
 
     if(position > 0){
         position--;
@@ -40,3 +47,423 @@ prevBtn.addEventListener('click', () => {
     `translateX(-${position * cardWidth}px)`;
 
 });
+
+const places = [
+    {
+        image: "images/stolby.jpg",
+        title: "Восточный вход Столбы",
+        description: "Исследовать",
+        link:"stolby.html",
+        tags:[
+        "view",
+        "nature",
+        "walk",
+        "family"
+    ]
+    },
+    {
+        image: "images/log.jpg",
+        title: "Серебряный лог",
+        description: "Исследовать",
+        link:"log.html",
+        tags:[
+        "nature",
+        "walk",
+        "family",
+        "culture"
+    ]
+    },
+    {
+        image: "images/ostrov.jpg",
+        title: "Остров Молокова",
+        description: "Исследовать",
+        link:"ostrov.html",
+        tags:[
+        "water",
+        "relax",
+        "family",
+        "easy"
+        ]
+    },
+    {
+        image: "images/dolina.png",
+        title: "Юдинская Долина",
+        description: "Исследовать",
+        link:"dolina.html",
+        tags:[
+        "nature",
+        "walk",
+        "family",
+        "relax"
+        ]
+    },
+    {
+        image: "images/griva.jpg",
+        title: "Гремячая Грива",
+        description: "Исследовать",
+        link:"griva.html",
+        tags:[
+        "nature",
+        "walk",
+        "sport",
+        "family"
+        ]
+    },
+    {
+        image: "images/sopka.webp",
+        title: "Николаевская Сопка",
+        description: "Исследовать",
+        link:"sopka.html",
+        tags:[
+        "view",
+        "photo",
+        "easy"
+        ]
+    },
+    {
+        image: "images/hrebet.webp",
+        title: "Торгашинский Хребет",
+        description: "Исследовать",
+        link:"hrebet.html",
+        tags:[
+        "view",
+        "sport",
+        "hard",
+        "photo"
+        ]
+    }
+];
+const questions = [
+    {
+        title:"Что хочется найти сегодня?",
+        answers : [
+    {
+        image:"images/test1.1.svg",
+        title:"Увидеть красивый вид",
+        text:"Панорамы и места с высоты",
+        tag:"view"
+    },
+    {
+        image:"images/test1.2.svg",
+        title:"Оказаться среди природы",
+        text:"Лес, скалы и тишина",
+        tag:"nature"
+    },
+    {
+        image:"images/test1.3.svg",
+        title:"Отдохнуть у воды",
+        text:"Спокойная прогулка без спешки",
+        tag:"water"
+    },
+    {
+        image:"images/test1.4.svg",
+        title:"Отправиться в путь",
+        text:"Маршруты и новые открытия",
+        tag:"sport"
+    }
+]
+    },
+
+    {
+        title:"Какой формат активности Вам ближе?",
+        answers : [
+    {
+        image:"images/test2.1.svg",
+        title:"Лёгкая прогулка",
+        text:"Гулять, смотреть, отдыхать",
+        tag:"walk"
+    },
+    {
+        image:"images/test2.2.svg",
+        title:"Активный маршрут",
+        text:"Тропы и прогулка с нагрузкой",
+        tag:"sport"
+    },
+    {
+        image:"images/test2.3.svg",
+        title:"Велосипед",
+        text:"Пространство для активной поездки",
+        tag:"bike"
+    },
+    {
+        image:"images/test2.4.svg",
+        title:"Отдых на природе",
+        text:"Пикники, пляжи, спокойное время",
+        tag:"relax"
+    }
+        ]
+    },
+    {
+        title:"Каким Вы видите свой маршрут?",
+        answers : [
+    {
+        image:"images/test3.1.svg",
+        title:"Лего и комфортно",
+        text:"Без серьёзных подъемов",
+        tag:"easy"
+    },
+    {
+        image:"images/test3.2.svg",
+        title:"Хочу подняться выше",
+        text:"Лестницы и подъемы",
+        tag:"view"
+    },
+    {
+        image:"images/test3.3.svg",
+        title:"Готов исследовать",
+        text:"Длинные маршруты и тропы",
+        tag:"hard"
+    },
+    {
+        image:"images/test3.4.svg",
+        title:"Без долгих маршрутов",
+        text:"Приехать, посмотреть и вернуться",
+        tag:"easy"
+    }
+        ]
+    },
+    {
+        title:"Что ещё важно для Вашей поездки?",
+        answers : [
+    {
+        image:"images/test4.1.svg",
+        title:"Красивые фотографии",
+        text:"Места с необычными видами и атмосферой",
+        tag:"photo"
+    },
+    {
+        image:"images/test4.2.svg",
+        title:"Увидеть закат",
+        text:"Лучшее время - вечерние часы",
+        tag:"view"
+    },
+    {
+        image:"images/test4.3.svg",
+        title:"Легко добраться",
+        text:"Без долгой подготовки и сложной дороги",
+        tag:"easy"
+    },
+    {
+        image:"images/test4.4.svg",
+        title:"Еду с семьёй или компанией",
+        text:"Комфортное место для совместного отдыха",
+        tag:"family"
+    }
+        ]
+    }
+];
+
+function initModal(){
+const modalContent = document.querySelector('.modal-content');
+const modalTest = document.querySelector('.modal-test');
+const resultBlock = document.querySelector('.result');
+const answersBlock = document.querySelector('.answers');
+
+const startBtn = document.querySelector('#BtnModal');
+
+const modal = document.querySelector('.constructor-modal');
+const closeModal = document.querySelector('#closeModal');
+
+const openButtons = document.querySelectorAll('.open-modal');
+
+openButtons.forEach(button => {
+
+    button.addEventListener('click',(e)=>{
+        e.preventDefault();
+
+        modal.classList.add('active');
+
+    });
+
+});
+
+
+closeModal.addEventListener('click',()=>{
+    modal.classList.remove('active');
+});
+
+startBtn.addEventListener('click',(e)=>{
+
+    e.preventDefault();
+
+    modalContent.classList.remove('active');
+
+    modalTest.classList.add('active');
+
+    renderQuestion();
+
+});
+
+/* ==========================
+   тест
+========================== */ 
+
+let currentQuestion = 0;
+let userAnswers = [];
+
+const stepText = document.querySelector('.step');
+function renderQuestion(){
+    stepText.textContent = `Шаг ${currentQuestion + 1} из 4`;
+}
+
+const testTitle = document.querySelector('.modal-test h1');
+
+
+function renderQuestion(){
+
+    const question = questions[currentQuestion];
+
+    stepText.textContent = `Шаг ${currentQuestion + 1} из 4`;
+
+    testTitle.textContent = question.title;
+
+
+    answersBlock.innerHTML = question.answers.map((answer,index)=>`
+
+        <button class="answer-card" data-tag="${answer.tag}">
+
+            <img src="${answer.image}">
+            <h3>${answer.title}</h3>
+            <p>${answer.text}</p>
+
+        </button>
+
+    `).join('');
+
+}
+renderQuestion();
+
+function showResult(){
+
+    modalTest.classList.remove('active');
+
+    resultBlock.classList.add('active');
+
+    stepText.textContent = "Результат";
+
+    renderResults();
+}
+
+    answersBlock.addEventListener('click',(event)=>{
+
+    const card = event.target.closest('.answer-card');
+
+    if(!card) return;
+
+    card.classList.add('selected');
+
+    userAnswers[currentQuestion] = card.dataset.tag;
+
+
+    console.log(userAnswers);
+
+
+    setTimeout(()=>{
+
+        if(currentQuestion < questions.length - 1){
+
+            currentQuestion++;
+            renderQuestion();
+
+        } else {
+
+            showResult();
+
+        }
+
+    }, 500);
+
+});
+
+const nextBtn = document.querySelector('.modal-next');
+nextBtn.addEventListener('click', ()=>{
+
+    if(currentQuestion < questions.length - 1){
+
+        currentQuestion++;
+        renderQuestion();
+
+    } else {
+
+        showResult();
+
+    }
+
+});
+
+const prevBtn = document.querySelector('.modal-prev');
+
+prevBtn.addEventListener('click',()=>{
+
+    if(currentQuestion > 0){
+
+        currentQuestion--;
+        renderQuestion();
+
+    } else {
+
+        modalTest.classList.remove('active');
+
+        modalContent.classList.add('active');
+
+        stepText.textContent = "";
+        userAnswers = [];
+
+    }
+
+});
+
+/* ==========================
+   карточки рез
+========================== */
+
+const modalCards = document.querySelector('.modal-cards');
+
+const resultBack = document.querySelector('.result-back');
+
+
+resultBack.addEventListener('click',()=>{
+
+    resultBlock.classList.remove('active');
+
+    modalTest.classList.add('active');
+
+    currentQuestion = questions.length - 1;
+
+    renderQuestion();
+
+});
+
+function getResults(){
+    let scores = places.map(place=>{
+        let score = 0;
+        userAnswers.forEach(answer=>{
+            if(place.tags.includes(answer)){
+                score++;
+            }
+        });
+        return {
+            ...place,
+            score};
+        });
+        return scores
+        .filter(place => place.score > 0)
+        .sort((a,b)=>b.score-a.score)
+        .slice(0,3);
+}
+
+function renderResults(){
+modalCards.innerHTML = getResults().map(place =>`
+    <div class="modal-card">
+        <img src="${place.image}">
+        <h3>${place.title}</h3>
+            <div class="result-card-footer">
+                <p>${place.description}</p>
+                    <a href="${place.link || 'javascript:void(0)'}" class="card-next">
+&rarr;
+</a>
+            </div>
+    </div>
+`).join('');
+}   
+}
